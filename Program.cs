977f7049fbe9e95e7d21b8643e1e56f384a4b575
Gayusara25 <111350059+Gayusara25@users.noreply.github.com>
2022-08-18 18:37:43 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace git2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            Console.Write("Enter a number:");
+            number = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("You entered :{0}", number);
+            Console.ReadLine();
+        }
+    }
+}
